5810287afbc32740923bd0bcb5b42d356ca117ea
AbdulloSodikov <sodikov.ak@gmail.com>
2020-10-16 10:46:52 +0500

    added tesk#3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int maxNum = max(15, 20);
+        int maxNum = max(15, 20);    // Задача № 2
+        int totalNumAB = totalNum(1, 6);   // Задача № 3
     }
 
     public static int max(int x, int y) {
@@ -12,5 +13,13 @@ public class Main {
         } else {
             return y;
         }
+    }
+
+    public static int totalNum(int a, int b) {
+        int counter = 0;
+        for (int i = a; i <= b; i++) {
+            counter = counter + i * i;
+        }
+        return counter;
     }
 }
