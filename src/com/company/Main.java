e3dc222f6ad83cf55f268a120756b3cd12e1160f
AbdulloSodikov <sodikov.ak@gmail.com>
2020-10-16 10:58:44 +0500

    added tesk#4

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
         int maxNum = max(15, 20);    // Задача № 2
         int totalNumAB = totalNum(1, 6);   // Задача № 3
+
+        int[] array = {10, 2, 3, 4, 5, 60, 7, 8, 9};  // Задача № 4
+        int maxNumberInArray = maxInArray(array);
+
     }
 
     public static int max(int x, int y) {
@@ -21,5 +25,14 @@ public class Main {
             counter = counter + i * i;
         }
         return counter;
+    }
+    public static int maxInArray(int[] array) {
+        int counterMax = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (counterMax < array[i]) {
+                counterMax = array[i];
+            }
+        }
+        return counterMax;
     }
 }
